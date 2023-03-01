99097c49c884b9ba6207ae7d733d85eec5699169
Ömer Göçmen <omer.gocmen@tekhnelogos.com>
2023-03-01 23:37:57 +0300

    cart component added

--- Client/react-client/src/pages/cart/cart.js
+++ Client/react-client/src/pages/cart/cart.js
@@ -1,47 +1,102 @@
 import React, { useState } from "react";
+import { Badge } from "primereact/badge";
+import Basebutton from "../../shared/components/baseButton";
+import { Rating } from "primereact/rating";
 
 export default function Cart() {
   const [dropdownOpen, setdropdownOpen] = useState(false);
-   
+
   return (
     <div>
       <button
-        onClick={()=>setdropdownOpen(!dropdownOpen)}
-        class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto"
+        onClick={() => setdropdownOpen(!dropdownOpen)}
+        className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto"
       >
-        Dropdown
-        <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill-rule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <i className="pi pi-shopping-cart p-overlay-badge" style={{ fontSize: "2rem" }}>
+          <Badge value="2"></Badge>
+        </i>
       </button>
       {dropdownOpen && (
-        <div class="bg-white text-base absolute z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
-          <ul class="py-1">
+        <div className="bg-white text-base absolute z-10 list-none divide-y divide-gray-100 rounded shadow w-[370px] max-w-[370px]">
+          <ul className="py-1 border border-2 rounded">
             <li>
-              <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">
-                Dashboard
-              </a>
+              <div className="flex flex-column xl:flex-row xl:align-items-start p-3">
+                <img
+                  className="w-9 max-w-[115px] min-h-[100px]"
+                  src={"https://kockerim.com/wp-content/uploads/2022/11/rabbitmq.png"}
+                  alt=".net core kursu"
+                />
+                <div className="text-left ml-4 flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                  <div className="flex flex-column align-items-center sm:align-items-start gap-y-2">
+                    <p className="font-bold text-900">RabbitMQ Masstransit Öğrenme Kursu</p>
+                    <div>
+                      <div>
+                        <Rating value={4} readOnly cancel={false}/>
+                        <i className="text-sm underline">Ömer Göçmen</i>
+                      </div>
+                        <span className="font-semibold">50$</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <hr/>
+            <li>
+              <div className="flex flex-column xl:flex-row xl:align-items-start p-3">
+                <img
+                  className="w-9 max-w-[115px] min-h-[100px]"
+                  src={"https://miro.medium.com/max/1400/0*n5APaWlshOK-IPNs.jpg"}
+                  alt=".net core kursu"
+                />
+                <div className="text-left ml-4 flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                  <div className="flex flex-column align-items-center sm:align-items-start">
+                    <p className="font-bold text-900 break-all">.Net Core Kurs</p>
+                    <div>
+                      Ömer Göçmen
+                      <div>
+                        <Rating value={2} readOnly cancel={false}/>
+                      </div>
+                        <span className="font-semibold">100$</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <hr/>
+            <li>
+              <div className="flex flex-column xl:flex-row xl:align-items-start p-3">
+                <img
+                  className="w-9 max-w-[115px] min-h-[115px]"
+                  src={"https://i.ytimg.com/vi/VESPItwumjw/sddefault.jpg"}
+                  alt=".net core kursu"
+                />
+                <div className="text-left ml-4 flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                  <div className="flex flex-column align-items-center sm:align-items-start">
+                    <p className="font-bold text-900">.Net Core Kursu</p>
+                    <div>
+                      Ömer Göçmen
+                      <div>
+                        <Rating value={3} readOnly cancel={false}/>
+                      </div>
+                        <span className="font-semibold">125$</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <hr/>
+            <li>
+              <div className="justify-between px-10 flex flex-column xl:flex-row xl:align-items-start p-3">
+                <p>Toplam Tutar</p>
+                <p>275$</p>
+              </div>
             </li>
             <li>
-              <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">
-                Earnings
-              </a>
+              <div className="justify-between px-10 flex flex-column xl:flex-row xl:align-items-start">
+              <Basebutton text="Ödemeye Geç"/> 
+              </div>
             </li>
           </ul>
-          <div class="py-1">
-            <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">
-              Sign out
-            </a>
-          </div>
         </div>
       )}
     </div>
